d7c911706bfc86b189dc71f0cbd08d94b47ccc05
johnkiernander <jkiernander@pmsi-consulting.com>
2015-04-12 00:35:00 +0100

    Added setAbsolutePosition tests

---
+++ test/spec/internal/dom/setAbsolutePosition.spec.js
@@ -0,0 +1,51 @@
+define(['scrollgrid_actual', 'scrollgrid_mock'], function (actual, mock) {
+    "use strict";
+
+    describe("setAbsolutePosition", function () {
+
+        var underTest = actual.prototype.internal.dom.setAbsolutePosition,
+            inShape,
+            outShape,
+            vals = {
+                x: 1,
+                y: 2,
+                width: 3,
+                height: 4
+            };
+
+        beforeEach(function () {
+            mock.init();
+            inShape = new mock.shape();
+            outShape = underTest.call(mock, inShape, vals.x, vals.y, vals.width, vals.height);
+        });
+
+        it("should return the passed object for chaining", function () {
+            expect(outShape).toEqual(inShape);
+        });
+
+        it("should set position to absolute", function () {
+            expect(outShape.styles.position).toEqual("absolute");
+        });
+
+        it("should set overflow to hidden", function () {
+            expect(outShape.styles.overflow).toEqual("hidden");
+        });
+
+        it("should set left to x value and add px for cross-browser compatibility", function () {
+            expect(outShape.styles.left).toEqual(vals.x + "px");
+        });
+
+        it("should set top to y value and add px for cross-browser compatibility", function () {
+            expect(outShape.styles.top).toEqual(vals.y + "px");
+        });
+
+        it("should set width to width value and add px for cross-browser compatibility", function () {
+            expect(outShape.styles.width).toEqual(vals.width + "px");
+        });
+
+        it("should set height to height value and add px for cross-browser compatibility", function () {
+            expect(outShape.styles.height).toEqual(vals.height + "px");
+        });
+
+    });
+});
