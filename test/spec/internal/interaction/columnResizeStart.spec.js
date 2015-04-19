00b17112d60b27389512d3a4f97bc755e6bfb1d5
johnkiernander <jkiernander@pmsi-consulting.com>
2015-04-19 01:41:38 +0100

    added columnResizeEnd tests

---
+++ test/spec/internal/interaction/columnResizeStart.spec.js
@@ -0,0 +1,26 @@
+define(['d3', 'scrollgrid_actual', 'scrollgrid_mock'], function (d3, actual, mock) {
+    "use strict";
+
+    describe("columnResizeStart", function () {
+
+        var underTest = actual.prototype.internal.interaction.columnResizeStart,
+            handle;
+
+        beforeEach(function () {
+            mock.init();
+            handle = new d3.shape();
+        });
+
+        it("should add the 'dragging' class", function () {
+            underTest.call(mock, handle);
+            expect(handle.classed).toHaveBeenCalledWith("dragging", true);
+        });
+
+        it("should stop event propagation", function () {
+            underTest.call(mock, handle);
+            expect(d3.event.sourceEvent.stopPropagation).toHaveBeenCalled();
+        });
+
+    });
+
+});
