bc93e813e0ab0310767bd8732bc0d53461d7ebf0
johnkiernander <jkiernander@pmsi-consulting.com>
2015-05-21 14:33:01 +0100

    Added CI task

--- Gruntfile.js
+++ Gruntfile.js
@@ -113,7 +113,7 @@ module.exports = function(grunt) {
 
     // Default tasks
     grunt.registerTask('default', ['concat', 'jslint', 'concat:test', 'karma:unit', 'uglify', 'copy', 'connect']);
-    grunt.registerTask('travis', ['concat', 'jslint', 'concat:test', 'karma:unit', 'coveralls']);
+    grunt.registerTask('travis', ['concat', 'jslint', 'concat:test', 'karma:unit']);
     grunt.registerTask('test:unit', ['concat:test', 'karma:unit']);
     grunt.registerTask('test', ['karma:continuous:start', 'watch']);
 
