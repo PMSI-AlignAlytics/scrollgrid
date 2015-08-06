38b401a546123e1545951a6ba9cbd7f0517fb521
johnkiernander <jkiernander@pmsi-consulting.com>
2015-08-06 15:21:26 +0100

    added initialiseColumns tests

--- test/spec/internal/sizes/initialiseColumns.spec.js
+++ test/spec/internal/sizes/initialiseColumns.spec.js
@@ -3,16 +3,120 @@ define(['d3', 'mock', 'sizes/initialiseColumns'], function (d3, mock) {
 
     describe("initialiseColumns", function () {
 
-        var underTest = Scrollgrid.prototype.internal.sizes.initialiseColumns;
+        var underTest = Scrollgrid.prototype.internal.sizes.physical.initialiseColumns;
 
         beforeEach(function () {
             mock.init();
             d3.init();
-
-            underTest();
-
         });
 
+        it("should use the default column width for the number of columns in the outerWidth if nothing is defined", function () {
+            delete mock.columns;
+            mock.internal.sizes.virtual.outerWidth = 5;
+            mock.internal.sizes.physical.defaultColumnWidth = 13;
+            underTest.call(mock);
+            expect(mock.columns.length).toEqual(5);
+            expect(mock.columns[0].width).toEqual(13);
+            expect(mock.columns[1].width).toEqual(13);
+            expect(mock.columns[2].width).toEqual(13);
+            expect(mock.columns[3].width).toEqual(13);
+            expect(mock.columns[4].width).toEqual(13);
+        });
+
+        it("should use and defined columns", function () {
+            mock.columns = [
+                { width: 7 },
+                null,
+                { width: 17 }
+            ];
+            mock.internal.sizes.virtual.outerWidth = 5;
+            mock.internal.sizes.physical.defaultColumnWidth = 13;
+            underTest.call(mock);
+            expect(mock.columns.length).toEqual(5);
+            expect(mock.columns[0].width).toEqual(7);
+            expect(mock.columns[1].width).toEqual(13);
+            expect(mock.columns[2].width).toEqual(17);
+            expect(mock.columns[3].width).toEqual(13);
+            expect(mock.columns[4].width).toEqual(13);
+        });
+
+        it("should not call matchRule if format rules are null", function () {
+            mock.internal.render.formatRules = null;
+            underTest.call(mock);
+            expect(mock.internal.render.matchRule).not.toHaveBeenCalled();
+        });
+
+        it("should not call matchRule if format rules are zero length", function () {
+            mock.internal.render.formatRules = [];
+            underTest.call(mock);
+            expect(mock.internal.render.matchRule).not.toHaveBeenCalled();
+        });
+
+        it("should call matchRule if format rules exist", function () {
+            mock.internal.render.formatRules = [{}];
+            underTest.call(mock);
+            expect(mock.internal.render.matchRule).toHaveBeenCalled();
+        });
+
+        it("should call use the column width rule if provided", function () {
+            delete mock.columns;
+            mock.internal.sizes.virtual.outerWidth = 5;
+            mock.internal.sizes.physical.defaultColumnWidth = 13;
+            mock.internal.render.matchRule.and.callFake(function (ruleColumn, compareColumn) {
+                return compareColumn === ruleColumn;
+            });
+            mock.internal.render.formatRules = [{
+                column: 3,
+                columnWidth: 31
+            }];
+            underTest.call(mock);
+            expect(mock.columns.length).toEqual(5);
+            expect(mock.columns[0].width).toEqual(13);
+            expect(mock.columns[1].width).toEqual(13);
+            expect(mock.columns[2].width).toEqual(31);
+            expect(mock.columns[3].width).toEqual(13);
+            expect(mock.columns[4].width).toEqual(13);
+        });
+
+        it("should use the sort rule if provided", function () {
+            delete mock.columns;
+            mock.internal.sizes.virtual.outerWidth = 5;
+            mock.internal.sizes.physical.defaultColumnWidth = 13;
+            mock.internal.render.matchRule.and.callFake(function (ruleColumn, compareColumn) {
+                return compareColumn === ruleColumn;
+            });
+            mock.internal.render.formatRules = [{
+                column: 3,
+                sort: 'asc'
+            }];
+            underTest.call(mock);
+            expect(mock.columns.length).toEqual(5);
+            expect(mock.columns[0].sort).toBeUndefined();
+            expect(mock.columns[1].sort).toBeUndefined();
+            expect(mock.columns[2].sort).toEqual('asc');
+            expect(mock.columns[3].sort).toBeUndefined();
+            expect(mock.columns[4].sort).toBeUndefined();
+        });
+
+        it("should use the compare function if provided", function () {
+            delete mock.columns;
+            mock.internal.sizes.virtual.outerWidth = 5;
+            mock.internal.sizes.physical.defaultColumnWidth = 13;
+            mock.internal.render.matchRule.and.callFake(function (ruleColumn, compareColumn) {
+                return compareColumn === ruleColumn;
+            });
+            mock.internal.render.formatRules = [{
+                column: 3,
+                compareFunction: 'Comparer'
+            }];
+            underTest.call(mock);
+            expect(mock.columns.length).toEqual(5);
+            expect(mock.columns[0].compareFunction).toBeUndefined();
+            expect(mock.columns[1].compareFunction).toBeUndefined();
+            expect(mock.columns[2].compareFunction).toEqual('Comparer');
+            expect(mock.columns[3].compareFunction).toBeUndefined();
+            expect(mock.columns[4].compareFunction).toBeUndefined();
+        });
 
     });
 
