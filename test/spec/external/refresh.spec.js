37191e50c9b5a2908656da43cfcf431dc0e06edb
johnkiernander <jkiernander@pmsi-consulting.com>
2015-08-07 23:06:15 +0100

    Added data tests

--- test/spec/external/refresh.spec.js
+++ test/spec/external/refresh.spec.js
@@ -8,9 +8,26 @@ define(['d3', 'mock', 'external/refresh'], function (d3, mock) {
         beforeEach(function () {
             mock.init();
             d3.init();
+        });
 
-            underTest();
+        it("should call layoutDOM", function () {
+            underTest.call(mock);
+            expect(mock.internal.dom.layoutDOM).toHaveBeenCalled();
+        });
 
+        it("should call draw with no clear cache if maintain cache is passed true", function () {
+            underTest.call(mock, true);
+            expect(mock.internal.render.draw).toHaveBeenCalledWith(false);
+        });
+
+        it("should call draw with clear cache if maintain cache is passed false", function () {
+            underTest.call(mock, false);
+            expect(mock.internal.render.draw).toHaveBeenCalledWith(true);
+        });
+
+        it("should call setScrollerSize", function () {
+            underTest.call(mock);
+            expect(mock.internal.dom.setScrollerSize).toHaveBeenCalled();
         });
 
     });
